d8e0e5f5e2fddbd80a800500e48b04777bdbfdbf
Camilo Sacanamboy <csacanam@gmail.com>
2022-10-22 22:48:44 -0500

    Removed unnecesary code in tests

--- packages/hardhat/test/lotero-test.js
+++ packages/hardhat/test/lotero-test.js
@@ -6,11 +6,6 @@ const provider = ethers.getDefaultProvider();
 describe("DApp Testing", function () {
   let myContract;
 
-  // quick fix to let gas reporter fetch data from gas station & coinmarketcap
-  before((done) => {
-    setTimeout(done, 2000);
-  });
-
   describe("Lotero Contract", function () {
 
     it("Should deploy Lotero contract", async function () {
