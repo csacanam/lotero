9e7daadce9eba3dca0541e5550fb7ad1d043a882
Camilo Sacanamboy <csacanam@gmail.com>
2022-10-22 21:13:58 -0500

    Removed comments on tests

--- packages/hardhat/test/lotero-test.js
+++ packages/hardhat/test/lotero-test.js
@@ -22,25 +22,12 @@ describe("DApp Testing", function () {
 
     describe("bet()", function () {
       it("Should be increased the total amount in the bet", async function () {
-        
+
         //Add 1 wei to bet 0 with number 1
         await myContract.bet(0, 1, { value: 1 });
 
         //Get first bet
         const bet = await myContract.bets(0);
-
-
-        //Print first bet object
-        //console.log(bet);
-
-        //Print money in first bet
-        //console.log(Number(await bet[0]));
-
-        //Print number of players in first bet
-        //console.log(Number(await bet[1]));addre
-
-        //Print winner number in first bet
-        //console.log(Number(await bet[2]));
 
         expect(Number(await bet[0])).to.be.equal(1);
       });
@@ -60,16 +47,7 @@ describe("DApp Testing", function () {
         expect(currentBalance).to.be.equal(previousBalance + 1);
 
       });
-
-      /*it("Should emit a SetPurpose event ", async function () {
-        const [owner] = await ethers.getSigners();
-
-        const newPurpose = "Another Test Purpose";
-
-        expect(await myContract.setPurpose(newPurpose))
-          .to.emit(myContract, "SetPurpose")
-          .withArgs(owner.address, newPurpose);
-      });*/
+      
     });
 
   });
