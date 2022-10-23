107afa0a63166366fbe451f9820f1b6b69040ca7
Camilo Sacanamboy <csacanam@gmail.com>
2022-10-22 21:05:00 -0500

    Fixed checkBetCouldBePayed in smart contract and fixed tests

--- packages/hardhat/contracts/Lotero.sol
+++ packages/hardhat/contracts/Lotero.sol
@@ -5,23 +5,30 @@ pragma solidity >=0.7.0 <0.9.0;
 contract Lotero {
 
     struct Player {
-        bool voted;  // if true, that person already voted
-        uint betId;   // index of the bet
-        uint256 amount; // player bet
+        bool voted;  //if true, that person already voted
+        uint betId;   //index of the bet
+        uint256 amount; //player bet
 
     }
 
     struct Bet {
-        uint256 amount; // number of money accumulated in the bet
+        uint256 amount; //number of money accumulated in the bet
         mapping(address => Player) players; //map of players in the bet
         mapping(uint8 => address []) playersByChoosenNumber; //map of addresses by bet number
         mapping(uint8 => uint256) amountByChoosenNumber;
-        uint256 numberOfPlayers; // number of players in the bet
+        uint256 numberOfPlayers; //number of players in the bet
         ValidNumber winnerNumber;
     }
 
-    enum ValidNumber {ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, NOT_VALID}
+    struct User {
+        uint256 user; //the user
+        uint256 moneyAdded; //money added to the contract by the user
+        uint256 moneyEarned; //money earned by the user
+        uint256 totalDebt; //amount of money the user can claim
+        bool active; //if true, user has activated the account
+    }
 
+    enum ValidNumber {ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, NOT_VALID}
 
     Bet[] public bets;
 
@@ -30,6 +37,13 @@ contract Lotero {
     uint8 public constant MAX_WIN_MULTIPLIER = 5;
 
     uint256 public activeBet;
+
+    mapping(uint256 => User) infoPerUser; //information per user
+    User [] users; //users
+
+    uint256 totalMoneyAdded; //total money added to the contract by users
+    uint256 totalMoneyEarned; //total money earned by users in the contract
+    uint256 totalBets; //total bets
 
     constructor () payable {
         Bet storage firstBet = bets.push();
@@ -62,6 +76,9 @@ contract Lotero {
         bets[betId].numberOfPlayers++;
         bets[betId].playersByChoosenNumber[betNumber].push(msg.sender);
         bets[betId].amountByChoosenNumber[betNumber] += currentPlayer.amount;
+
+        //Update general stats
+        totalMoneyAdded += currentPlayer.amount;
     }
 
     /**
@@ -115,10 +132,10 @@ contract Lotero {
         //Get de winning multiplier (from 2 to 5)
         uint8 winningMultiplier = getWinnerMultiplier(winningNumber);
 
-        //Pay to winners
+        //Pay to winners - Fix this. The contract should not pay to winners. Winners should claim their earnings.
         for(uint8 i = 0; i < winners.length; i++) { 
-            address payable winner = payable(winners[i]);
-            winner.transfer(bets[activeBet].players[winner].amount * winningMultiplier);
+            //address payable winner = payable(winners[i]);
+            //winner.transfer(bets[activeBet].players[winner].amount * winningMultiplier);
         }
 
         //Increase bet index
@@ -244,9 +261,9 @@ contract Lotero {
     *@param amount the amount to be validated
     */
     modifier checkBetCouldBePayed(uint betId, uint256 amount) {
+        require(amount <= 10 ether, "Amount should be equal or less than 10");
         uint256 possibleNewAmount = amount + getMaxBetAmountInBet(betId);
-        //@TODO Check if the multiplier should be added again
-        require(address(this).balance >= possibleNewAmount, "Not enough money in balance to add this bet");
+        require(address(this).balance >= possibleNewAmount * MAX_WIN_MULTIPLIER, "Not enough money in contract to add this bet");
         _;
     }
 
--- packages/hardhat/test/lotero-test.js
+++ packages/hardhat/test/lotero-test.js
@@ -16,15 +16,15 @@ describe("DApp Testing", function () {
     it("Should deploy Lotero contract", async function () {
       const Lotero = await ethers.getContractFactory("Lotero");
 
-      myContract = await Lotero.deploy();
+      myContract = await Lotero.deploy({ value: 10 });
+
     });
 
     describe("bet()", function () {
       it("Should be increased the total amount in the bet", async function () {
-        //const [owner] = await ethers.getSigners();
-
-        //Add 5 wei to bet 0 with number 1
-        await myContract.bet(0, 1, { value: 5 });
+        
+        //Add 1 wei to bet 0 with number 1
+        await myContract.bet(0, 1, { value: 1 });
 
         //Get first bet
         const bet = await myContract.bets(0);
@@ -37,12 +37,12 @@ describe("DApp Testing", function () {
         //console.log(Number(await bet[0]));
 
         //Print number of players in first bet
-        //console.log(Number(await bet[1]));
+        //console.log(Number(await bet[1]));addre
 
         //Print winner number in first bet
         //console.log(Number(await bet[2]));
 
-        expect(Number(await bet[0])).to.be.equal(5);
+        expect(Number(await bet[0])).to.be.equal(1);
       });
 
       it("Should be increased the total amount in contract", async function () {
@@ -52,12 +52,12 @@ describe("DApp Testing", function () {
 
         const previousBalance = Number(await ethers.provider.getBalance(myContract.address));
 
-        //Add 5 wei to bet 0 with number 1
-        await contractAsAccount2.bet(0, 1, { value: 5 });
+        //Add 1 wei to bet 0 with number 1
+        await contractAsAccount2.bet(0, 1, { value: 1 });
 
         const currentBalance = Number(await ethers.provider.getBalance(myContract.address));
 
-        expect(currentBalance).to.be.equal(previousBalance + 5);
+        expect(currentBalance).to.be.equal(previousBalance + 1);
 
       });
 
