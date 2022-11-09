a4c7a55eaaadb62cab89243e4abb7a857f4a7a8a
Miguel Jimenez <1284036+jachinte@users.noreply.github.com>
2022-11-08 21:26:36 -0500

    Configure prettier

---
+++ packages/react-app/prettier.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  embeddedLanguageFormatting: "auto",
+  endOfLine: "lf",
+  htmlWhitespaceSensitivity: "css",
+  printWidth: 120,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+};
